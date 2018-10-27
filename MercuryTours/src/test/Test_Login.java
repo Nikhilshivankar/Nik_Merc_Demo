890100303e128ec3d984231cb1cd7107b7c8b446
Nikhil Shivankar <nikhilshivankar50@gmail.com>
2018-10-27 14:39:46 +0530

    updated Testlogin class and testNGxml

--- MercuryTours/src/test/Test_Login.java
+++ MercuryTours/src/test/Test_Login.java
@@ -47,4 +47,194 @@ public class Test_Login {
 		homePage.fun_login("shivanand","mercury");
 		driver.close();
 	}
+	
+	@Test(groups = "testLogin" )
+	public void test_login3() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+
+
+	@Test(groups = "testLogin" )
+	public void test_login4() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "testLogin" )
+	public void test_login5() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "regression" )
+	public void test_login6() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "regression" )
+	public void test_login7() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "regression" )
+	public void test_login8() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "regression" )
+	public void test_login9() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "smoke" )
+	public void test_login10() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "smoke" )
+	public void test_login11() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "smoke" )
+	public void test_login12() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "smoke" )
+	public void test_login13() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "golden TC" )
+	public void test_login14() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "golden TC" )
+	public void test_login15() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "golden TC" )
+	public void test_login16() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "golden TC" )
+	public void test_login17() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "golden TC" )
+	public void test_login18() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
+	@Test(groups = "golden TC" )
+	public void test_login19() {
+		
+		browsermanager = new BrowserManager(driver);	
+	
+		driver = browsermanager.navigateToURL();
+		
+		homePage = new HomePage(driver);
+		homePage.fun_login("mercury","mercury");
+		driver.close();
+	}
 }
